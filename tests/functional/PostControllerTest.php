1d4b49f12634b694d8988c3ab81104e3abae501a
konark kapil <konarkkapil@rocketmail.com>
2021-01-27 18:02:30 +0530

    added more functional tests

--- tests/functional/PostControllerTest.php
+++ tests/functional/PostControllerTest.php
@@ -32,7 +32,19 @@ class PostControllerTest extends WebTestCase
     public function testDoesHomeLoads(){
         $client = static::createClient();
         $client->request('GET', "/");
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
 
+    /**
+     * @test
+     */
+    public function testLogin(){
+        $client = static::createClient();
+        $client->request("GET", "/login", [
+            "email" => "konarkkapil2@gmail.com",
+            "password" => "konark"
+        ]);
+        echo($client->getResponse());
         $this->assertTrue($client->getResponse()->isSuccessful());
     }
 }
